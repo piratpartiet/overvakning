ea0d96231ddb1d7281bf077960004029aaa089d8
Egil Moeller <egil.moller@freecode.no>
2014-02-18 22:49:03 +0100

    Stories are now loaded from json

--- timeline.js
+++ timeline.js
@@ -1,21 +1,78 @@
 $(document).ready(function () {
-  $(".story").each(function () {
-    (function (story) {
-      var timeout;
-      story.mouseenter(function () {
-        if (timeout) clearTimeout(timeout);
-        timeout = setTimeout(function () {
-          timeout = undefined;
-          story.find(".more").slideDown();
-        }, 500);
-      });
-      story.mouseleave(function () {
-        if (timeout) clearTimeout(timeout);
-        timeout = setTimeout(function () {
-          timeout = undefined;
-          story.find(".more").slideUp();
-        }, 500);
-      });
-    })($(this));
-  });
+  $.get("stories.json", function (data) {
+    var timelines = {};
+    var idx = 0;
+    data.timelines.map(function (timeline) {
+      timeline.idx = idx;
+      timelines[timeline.title] = timeline;
+      var eltimeline = $(".template .timeline").clone();
+      eltimeline.addClass(timeline.color);
+      eltimeline.prepend(timeline.title);
+      $(".timelines").append(eltimeline);
+      idx++;
+    });
+
+    data.stories.map(function (story) {
+      var timeline = timelines[story.timeline];
+
+      var elstory = $(".template .story").clone();
+      elstory.addClass(timeline.color);
+      elstory.find(".time .main").html(story.time);
+      elstory.find(".info").prepend(story.title);
+      elstory.find(".info .more").html(story.content);
+
+      if (timeline.idx < data.timelines.length / 2) {
+        elstory.addClass("left");
+
+        for (var i = 0; i < timeline.idx; i++) {
+          elstory.find(".time").append("<div class='timeline-bump'>");
+          elstory.find(".info-wrapper").append("<div class='timeline-bump'>");
+        }
+
+        for (var i = 0; i < data.timelines.length / 2 - timeline.idx - 1; i++) {
+          elstory.append("<div class='timeline-bump'>");
+        }
+
+        elstory.append("<div class='even-bump'>");
+        elstory.append("<div class='side-bump'>");
+      } else {
+        elstory.addClass("right");
+
+        for (var i = 0; i < data.timelines.length - timeline.idx - 1; i++) {
+          elstory.find(".time").prepend("<div class='timeline-bump'>");
+          elstory.find(".info-wrapper").prepend("<div class='timeline-bump'>");
+        }
+
+        for (var i = 0; i < timeline.idx - data.timelines.length / 2; i++) {
+          elstory.prepend("<div class='timeline-bump'>");
+        }
+
+        elstory.prepend("<div class='even-bump'>");
+        elstory.prepend("<div class='side-bump'>");
+      }
+
+      (function (story) {
+        var timeout;
+        var info = story.find(".info");
+        info.mouseenter(function () {
+          if (timeout) clearTimeout(timeout);
+          timeout = setTimeout(function () {
+            timeout = undefined;
+            story.find(".more").slideDown();
+          }, 500);
+        });
+        info.mouseleave(function () {
+          if (timeout) clearTimeout(timeout);
+          timeout = setTimeout(function () {
+            timeout = undefined;
+            story.find(".more").slideUp();
+          }, 500);
+        });
+      })(elstory);
+
+      $(".stories .inner-stories").append(elstory);
+    });
+
+
+  }, "json");
 });
